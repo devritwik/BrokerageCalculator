fb875be297304b264fc41b7f9d353c8b8d04a34e
Ritwik Das <ritwikdas@Ritwiks-MacBook-Air.local>
2023-05-13 13:54:16 +0530

    Footer Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Main from './components/Main';
+import Footer from './components/Footer';
 import logo from './logo.svg';
 import './App.css';
 
 
 function App() {
   return (
+    <div>
     <div className="App">
       <h1>Brokerage Calculator</h1>
       <Main/>
+    </div>
+    <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+import './Footer.css'
+function Footer(){
+    return(
+        <footer>
+            <p><b>Made with ❤️ by <a href="https://github.com/devritwik">Ritwik</a></b></p>
+        </footer>
+    );
+}
+
+export default Footer;
